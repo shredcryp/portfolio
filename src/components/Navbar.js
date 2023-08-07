025bb63d91bea30121f91bc5325b8426b9f37281
shredcryp <shreedhar7bpatil@gmail.com>
2023-08-07 14:00:55 +0530

    Navbar icons fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import {useRef} from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa'
 import './Navbar.css';
 
 function Navbar() {
+
+    const navRef = useRef()
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav")
+    }
+
     return (
-        <nav>
+        <nav ref={navRef}>
             <div className="navbar-left">
                 <div className="wrapper">
                     <div className="typing-demo">
@@ -15,6 +23,7 @@ function Navbar() {
                 <h3>GitHub</h3>
                 <h3>LinkedIn</h3>
                 <h3>Contact</h3>
+                <button><FaTimes /></button>
             </div>
         </nav>
     );
