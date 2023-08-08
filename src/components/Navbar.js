a1bd15880f87dc6298d973b3c9e3593d645666cf
shredcryp <shreedhar7bpatil@gmail.com>
2023-08-08 17:26:31 +0530

    Responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import {useRef} from 'react';
-import {FaBars, FaTimes} from 'react-icons/fa'
+import { useRef, useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import './Navbar.css';
 
 function Navbar() {
+    const navRef = useRef();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const navRef = useRef()
-
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav")
-    }
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
-        <nav ref={navRef}>
+        <nav ref={navRef} className={isMenuOpen ? 'responsive_nav' : ''}>
             <div className="navbar-left">
                 <div className="wrapper">
                     <div className="typing-demo">
@@ -19,12 +19,13 @@ function Navbar() {
                     </div>
                 </div>
             </div>
-            <div className="navbar-right">
+            <div className={`navbar-right ${isMenuOpen ? 'show' : ''}`}>
                 <h3>GitHub</h3>
                 <h3>LinkedIn</h3>
                 <h3>Contact</h3>
-                <button><FaTimes /></button>
             </div>
+            <button className="hamburger" onClick={toggleMenu}><FaBars /></button>
+            <button className="cross" onClick={toggleMenu}><FaTimes /></button>
         </nav>
     );
 }
